459955d78d580276e8197d81e7abb93ec91a1465
AryanVerma <sfeng.pawanverma@gmail.com>
2016-08-02 18:51:50 +0530

    Add files via upload

---
+++ ClearCurrentPopup.xaml.cs
@@ -0,0 +1,84 @@
+﻿
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class ClearCurrentPopup : PopupPage
+    {
+        bool imageTapped = false;
+        public ClearCurrentPopup()
+        {
+            InitializeComponent();
+            Animation = new UserAnimation();
+            NavigationPage.SetHasBackButton(this, false);
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        protected override void OnAppearing()
+        {
+            if (Device.OS == TargetPlatform.Android)
+            {
+                CheckOrUnCheckImg.TranslationX = 90;
+                ClearFileInfoLabel.FontSize = 30;
+                ClearFileInfoLabel.TranslationX = 120;
+            }
+            if (Device.OS == TargetPlatform.Windows || Device.OS == TargetPlatform.WinPhone)
+            {
+                OKButton.IsEnabled = false;
+                if (OKButton.IsEnabled)
+                {
+                    btnLayout.BackgroundColor = Color.Green;
+                }
+                OKButton.WidthRequest = 100;
+                CancelButton1.WidthRequest = 100;
+                ClearFileInfoLabel.TranslationX = 130;
+                CheckOrUnCheckImg.TranslationX = 100;
+            }
+            base.OnAppearing();
+        }
+
+        private async void OnOk(object sender, EventArgs e)
+        {
+
+          
+                await PopupNavigation.PopAsync(false);
+           
+
+
+        }
+
+        private async void OnCancel(object sender, EventArgs e)
+        {
+          
+           
+                await PopupNavigation.PopAsync(false);
+            
+        }
+        private async void OnClose(object sender, EventArgs e)
+        {
+            await PopupNavigation.PopAsync();
+        }
+        void CheckMarkTapped(object sender, EventArgs args)
+        {
+            if (imageTapped == false)
+            {
+                CheckOrUnCheckImg.Source = "check.png";
+                imageTapped = true;
+                OKButton.BackgroundColor = Color.FromHex("70B856");
+                OKButton.IsEnabled = true;
+            }
+
+            else
+            {
+                OKButton.IsEnabled = false;
+                OKButton.BackgroundColor = Color.Silver;
+                CheckOrUnCheckImg.Source = "uncheck.png";
+                imageTapped = false;
+            }
+        }
+
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Extensions;
+using Xamarin.Forms;
+
+namespace App3
+{
+	public partial class Page1 : ContentPage
+	{
+        public static Image clearBtn;     
+       
+        public static TapGestureRecognizer clearBtnGesture;
+        public Page1 ()
+		{
+			InitializeComponent ();
+		}
+        protected async void clearTap(object sender, EventArgs args)
+        {
+            try
+            {
+                                  
+                    await Navigation.PushPopupAsync(new ClearCurrentPopup());
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
